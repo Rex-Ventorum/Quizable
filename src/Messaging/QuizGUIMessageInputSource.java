e633b6acb51ac1e6277301f91ef64574f954b4c2
Brandon <Brandon@NubCenter.attlocal.net>
2017-10-02 13:48:39 -0500

    Messaging: Created QuizGUIMessageInputSource

--- src/Messaging/QuizGUIMessageInputSource.java
+++ src/Messaging/QuizGUIMessageInputSource.java
@@ -1,17 +1,30 @@
 package Messaging;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Brandon
  */
 public class QuizGUIMessageInputSource implements MessageInputSource{
+    private static final String DEFAULT_QUESTION = "<No Question>";
+    private String quizQuestion;
     
     public QuizGUIMessageInputSource(){
-        
+        quizQuestion = DEFAULT_QUESTION;
     }
     
-    public String recieveMessage() {
-
+    @Override
+    public final String recieveMessage() {
+        return JOptionPane.showInputDialog(quizQuestion);
+    }
+   
+    public final void setQuestQuestion(String question){
+        if(question == null) throw new IllegalArgumentException("Question May Not Be Null");
+        quizQuestion = question;
     }
     
+    public final String getQuizQuestion(){
+        return quizQuestion;
+    }
 }
